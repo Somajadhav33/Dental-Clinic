e30a93012b20e0d3a30400d49ece625fcdca80c7
Soma <jadhavsomnath7005@gmail.com>
2026-01-16 11:42:40 +0530

    feat(route): admin-panel

---
+++ app/admin-panel/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+import AdminPageContent from "@/components/admin_models/AdminPageContent";
+import TopBar from "@/components/admin_models/TopBar";
+import React, { useState } from "react";
+
+export default function Page() {
+  const [activeTab, setActiveTab] = useState("");
+  return (
+    <>
+      <TopBar />
+      <div>
+        <AdminPageContent />
+      </div>
+    </>
+  );
+}
