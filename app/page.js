1e15412d325d7cabbcf67f8b39df16a4f9b2c746
Soma <jadhavsomnath7005@gmail.com>
2026-01-27 19:08:27 +0530

    refactor(page.js):  introduced new pages

--- app/page.js
+++ app/page.js
@@ -6,14 +6,17 @@ import { NavigationBar } from "@/components/NavBar";
 import ServiceCard from "@/components/ServiceCard";
 import initializeDb from "@/lib/db";
 import services from "@/SampleData/ServicesData";
-
+import { data } from "@/SampleData/Faq";
+import FAQComponent from "@/components/FaqS";
+import ReviewsComponent from "@/components/Reviews";
+import Footer from "@/components/Footer";
 export default function Home() {
   // initializeDb();
   return (
     <>
       <Hero />
       <HospitalImageGallery />
-      <div id ="servicesSection"className="text-center mb-12">
+      <div id="servicesSection" className="text-center mb-12">
         <h2 className="text-4xl font-bold text-blue-900 font-serif">
           Our Services
         </h2>
@@ -25,6 +28,12 @@ export default function Home() {
         ))}
       </div>
 
+      <div id="ReviewsSection" className="text-center mb-12">
+        <h2 className="text-4xl font-bold text-blue-900 font-serif">Reviews</h2>
+        <div className="w-24 h-1 bg-cyan-500 mx-auto mt-4 rounded-full"></div>
+      </div>
+      <ReviewsComponent />
+
       <div id="aboutSection" className="text-center mb-12">
         <h2 className="text-4xl font-bold text-blue-900 font-serif">
           About Us
@@ -33,13 +42,20 @@ export default function Home() {
       </div>
       <AboutPage />
 
-      <div id="contactSection"className="text-center mb-12">
+      <div id="FaqSection" className="text-center mb-12">
+        <h2 className="text-4xl font-bold text-blue-900 font-serif">FAQ</h2>
+        <div className="w-24 h-1 bg-cyan-500 mx-auto mt-4 rounded-full"></div>
+      </div>
+      <FAQComponent />
+
+      <div id="contactSection" className="text-center mb-12">
         <h2 className="text-4xl font-bold text-blue-900 font-serif">
           Contact Us
         </h2>
         <div className="w-24 h-1 bg-cyan-500 mx-auto mt-4 rounded-full"></div>
       </div>
       <ContactUsForm />
+      <Footer />
     </>
   );
 }
