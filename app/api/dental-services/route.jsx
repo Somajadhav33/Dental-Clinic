93407c852a6b7647e4df67bf4370f3f65c310775
Soma <jadhavsomnath7005@gmail.com>
2026-01-31 10:29:34 +0530

    feat(api: dental-services): introduce PUT, POST route

--- app/api/dental-services/route.jsx
+++ app/api/dental-services/route.jsx
@@ -14,4 +14,38 @@ export async function GET() {
   );
 }
 
-export async function POST(request) {}
+export async function POST(request) {
+  const { name, category, description, image_url, is_active, display_order } =
+    await request.json();
+  const result = await db.query(
+    "INSERT INTO services(name , category, description, image_url, is_active, display_order ) VALUES($1, $2, $3, $4, $5, $6)",
+    [name, category, description, image_url, is_active, display_order],
+  );
+  if (result.rowCount === 0) {
+    throw new Error("Error while creatig new service");
+  }
+  return NextResponse.json(
+    {
+      services: result,
+    },
+    { status: 200 },
+  );
+}
+
+export async function PUT(request) {
+  const { id, name, category, description, image_url, is_active } =
+    await request.json();
+  const result = await db.query(
+    "UPDATE services SET name = $1, category = $2, description = $3, image_url= $4, is_active=$5 WHERE id=$6",
+    [name, category, description, image_url, is_active, id],
+  );
+  if (result.rowCount === 0) {
+    throw new Error("Error While Updating");
+  }
+  return NextResponse.json(
+    {
+      services: result,
+    },
+    { status: 200 },
+  );
+}
