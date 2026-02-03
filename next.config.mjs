2c8bd5b335cce9a5134fea6ae78d08205427c367
Soma <jadhavsomnath7005@gmail.com>
2026-02-03 22:08:07 +0530

    feat(dependences): Updated dependences

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,30 @@ import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 const nextConfig = {
   images: {
     remotePatterns: [
+      // Local dev
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "3000",
+      },
+
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+      },
       {
         protocol: "https",
         hostname: "*",
