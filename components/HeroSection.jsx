9b814abb016e3171bffc0e79c3dd75d65ab9c03e
Soma <jadhavsomnath7005@gmail.com>
2026-02-03 11:29:48 +0530

    UI(HeroSection): changed link colors emergency call

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import { ShieldAlert } from "lucide-react";
 
 export default function Hero() {
   return (
     <section className="relative min-h-screen flex items-center bg-white text-slate-900 overflow-hidden">
-      {" "}
       <div className="absolute inset-0">
         <Image
           src="/hero-2.jpeg"
@@ -16,11 +16,14 @@ export default function Hero() {
         <div className="absolute inset-0 bg-linear-to-r from-white via-white/90 to-white/20" />
       </div>
       <div className="absolute top-8 right-8 bg-red-600 text-white px-6 py-4 rounded-lg shadow-xl font-semibold text-lg z-10 animate-bounce">
-        Emergency? Call: XXX XXXX XXXXX
+        <a href="tel:12345678901" className="flex items-center gap-2">
+          <ShieldAlert />
+          Emergency? Call: 123 4567 8901
+        </a>
       </div>
+
       <div className="relative z-10 max-w-7xl mx-auto px-6 py-20">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
-          {/* Text Content */}
           <div className="space-y-8">
             <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold leading-tight font-serif text-slate-900">
               Smile Brighter,
@@ -37,15 +40,15 @@ export default function Hero() {
             <div className="flex flex-col sm:flex-row gap-6 pt-4">
               <Link
                 href="/book-appointment"
-                className="inline-block bg-primary-500 text-white font-semibold text-md px-8 py-4 rounded-lg shadow-lg hover:bg-blue-700 hover:shadow-xl hover:-translate-y-1 transition-all duration-300 text-center"
+                className="inline-block bg-teal-600 text-white font-semibold text-md px-8 py-4 rounded-lg shadow-lg hover:bg-teal-700 hover:shadow-xl hover:-translate-y-1 transition-all duration-300 text-center"
               >
                 Book an Appointment
               </Link>
               <Link
                 href="/appointment-status"
-                className="inline-block bg-white border-2 border-slate-200 text-slate-700 font-semibold text-md px-8 py-4 rounded-lg hover:border-blue-600 hover:text-blue-600 transition-all duration-300 text-center"
+                className="inline-block bg-white border-2 border-slate-200 text-slate-700 font-semibold text-md px-8 py-4 rounded-lg hover:border-teal-600 hover:-translate-y-1  transition-all duration-300 text-center"
               >
-                Check Appointment Status/report
+                Check Appointment Status/ Report
               </Link>
             </div>
           </div>
