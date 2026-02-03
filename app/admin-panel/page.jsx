c9f38628535d2c6a2332dbbd21b72569be70e1e2
Soma <jadhavsomnath7005@gmail.com>
2026-02-03 22:01:55 +0530

    refactor(admin-panel): page authentication using cookie + jwt

--- app/admin-panel/page.jsx
+++ app/admin-panel/page.jsx
@@ -1,9 +1,37 @@
 "use client";
-import AdminPageContent from "@/components/admin_models/AdminPageContent";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 import TopBar from "@/components/admin_models/TopBar";
-import React, { useState } from "react";
+import AdminPageContent from "@/components/admin_models/AdminPageContent";
 
 export default function Page() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const jwtToken = Cookies.get("jwtToken");
+
+    if (!jwtToken) {
+      router.push("/admin-login");
+      return;
+    }
+
+    const validate = async () => {
+      const response = await fetch("/api/verify", {
+        headers: {
+          Authorization: `Bearer ${jwtToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        router.push("/admin-login");
+      }
+    };
+
+    validate();
+  }, [router]);
+
   return (
     <>
       <TopBar />
