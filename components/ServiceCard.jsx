4ddf6b00a493c05276505378c1c360255418c635
Soma <jadhavsomnath7005@gmail.com>
2026-02-03 11:36:59 +0530

    UI(ServiceCard): changed cursor

--- components/ServiceCard.jsx
+++ components/ServiceCard.jsx
@@ -21,7 +21,6 @@ export default async function ServiceCard() {
           key={id}
           className="flex flex-col md:flex-row bg-[#fdf7f3] rounded-2xl border border-orange-200 overflow-hidden w-full hover:shadow-md transition-shadow"
         >
-          {/* Image */}
           <div className="w-full mt-1.5 md:w-56 lg:w-55 rounded-2xl h-48 bg-[#e6dcd2] shrink-0 border-b md:border-b-0 md:border-r border-orange-100 flex items-center justify-center">
             <Image
               src={image_url}
@@ -33,7 +32,6 @@ export default async function ServiceCard() {
             />
           </div>
 
-          {/* Content */}
           <div className="p-5 md:p-6 flex flex-col flex-1 justify-between gap-4">
             <div className="space-y-2">
               <p
@@ -57,7 +55,7 @@ export default async function ServiceCard() {
 
             <div className="flex justify-end">
               <Link href={`/book-appointment?service=${name}`}>
-                <button className="rounded-sm text-white bg-orange-500 hover:bg-orange-600 px-4 py-1.5 text-xs font-bold uppercase shadow-sm">
+                <button className="rounded-sm text-white bg-orange-500 cursor-pointer hover:bg-orange-600 px-4 py-1.5 text-xs font-bold uppercase shadow-sm">
                   Book Now
                 </button>
               </Link>
@@ -68,54 +66,3 @@ export default async function ServiceCard() {
     </div>
   );
 }
-
-// import Image from "next/image";
-// import Link from "next/link";
-
-// export default function ServiceCard({ serviceData }) {
-//   const { name, description, image_url, is_active } = serviceData;
-//   return (
-//     <div className="flex flex-col md:flex-row bg-[#fdf7f3] rounded-2xl border border-orange-200 overflow-hidden w-full min-h-250px hover:shadow-md transition-shadow">
-//       <div className="w-full md:w-56 lg:w-64 h-48 bg-[#e6dcd2] shrink-0 border-b md:border-b-0 md:border-r border-orange-100 flex items-center justify-center">
-//         <Image
-//           src={image_url}
-//           alt={name}
-//           width={300}
-//           height={300}
-//           className="object-contain w-full h-full p-4"
-//           priority
-//         />
-//       </div>
-
-//       <div className="p-5 md:p-6 flex flex-col flex-1 justify-between gap-4">
-//         <div className="space-y-2">
-//           <p
-//             className={
-//               is_active
-//                 ? "text-green-600 text-sm font-semibold uppercase tracking-wider"
-//                 : "text-red-500 text-sm font-semibold uppercase tracking-wider"
-//             }
-//           >
-//             {is_active ? "Available" : "Not available"}
-//           </p>
-//           <div>
-//             <h2 className="text-xl font-bold text-gray-900 line-clamp-1">
-//               {name}
-//             </h2>
-//             <p className="text-gray-600 mt-1 text-sm line-clamp-3 leading-relaxed">
-//               {description}
-//             </p>
-//           </div>
-//         </div>
-
-//         <div className="flex flex-row justify-end items-center gap-3 mt-2">
-//           <Link href={`/book-appointment?service=${name}`}>
-//             <button className="rounded-sm text-white bg-orange-500 hover:bg-orange-600 px-4 py-1.5 h-auto text-xs font-bold uppercase shadow-sm">
-//               Book Now
-//             </button>
-//           </Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
